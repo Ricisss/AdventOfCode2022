aec41acab79f15da7a442ffd73d62a47e93bfaf8
Richard Smith <qpalymmm@gmail.com>
2022-12-07 19:31:33 +0100

    Solution 7

---
+++ adventofcode2022/Solutions/7/Solution.ts
@@ -0,0 +1,23 @@
+
+import _ = require("underscore");
+
+
+
+export class Solution {
+
+
+
+
+    constructor(input: string) {
+        this._parseInput(input);
+    }
+
+    public GetSolution(): any {
+
+    }
+
+
+    private _parseInput(input: string) {
+        const lines = input.split("\n");
+    }
+}
